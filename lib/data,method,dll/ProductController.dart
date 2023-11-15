9cce830f9b93e3a6a00ef0aa9b0196aa1a740dd4
zelphyx <rizqy.astiko19@gmail.com>
2023-11-15 12:01:23 +0700

    fixed [IR] UI Homepage

--- lib/data,method,dll/ProductController.dart
+++ lib/data,method,dll/ProductController.dart
@@ -1,10 +1,19 @@
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'EcommerceListModel.dart';
 
 
 class ControllerProduct extends GetxController{
   SharedPreferences? prefs;
+
+RxList<Ecommercelistmodel>listmodelctr =
+<Ecommercelistmodel>[].obs;
+  RxBool isLoading = true.obs;
+
+
+
+
 
   RxString sessionUsername = "".obs;
   void loadSession() async{
@@ -13,11 +22,27 @@ class ControllerProduct extends GetxController{
     print("get username "+ sessionUsername.value.toString());
   }
 
+  void loadData()async {
+
+    final response =await http.get(Uri.parse("https://fakestoreapi.com/products"));
+
+    if(response.statusCode == 200){
+      listmodelctr.value = ecommercelistmodelFromJson(response.body);
+      print("halo: ${listmodelctr.value}");
+    } else{
+    print("status code : " + response.statusCode.toString());
+    }
+
+    isLoading.value = false;
+
+  }
+
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     loadSession();
+    loadData();
   }
 
 }
