f2ea28d8997c3831c2f978c8f7ec73a730b65ce2
Maida Hesti <hestimaida50@gmail.com>
2023-11-26 09:23:27 +0700

    [MH]Style/cart

--- lib/Cart/cart_controller.dart
+++ lib/Cart/cart_controller.dart
@@ -1,76 +1,76 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:pas_kelas11/data,method,dll/EcommerceListModel.dart';
-//
-// class CartController extends GetxController{
-//   RxList<Ecommercelistmodel> cartlist = <Ecommercelistmodel>[].obs;
-//   RxBool isload = false.obs;
-//   var isGopayChecked = false.obs;
-//
-//   void changeGopayValue(bool value) {
-//     isGopayChecked.value = value;
-//
-//   }
-//
-//   // double updateTotalPrice(BuildContext context, double totalPrice){
-//   //   for (var item in cartlist) {
-//   //     totalPrice += (item.price ?? 0.0) * (item.quantity ?? 1); // Calculate total price based on item prices and quantities
-//   //   }
-//   //   return totalPrice;
-//   // }
-//
-//   addToCart(BuildContext context, Ecommercelistmodel newItem) {
-//     bool isExisting = false;
-//
-//     for (var item in cartlist.value) {
-//       if (item.id == newItem.id) {
-//         isExisting = true;
-//         break;
-//       }
-//     }
-//
-//     if (!isExisting) {
-//       cartlist.value.add(newItem);
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text('Item added to cart.'),
-//           duration: Duration(seconds: 2),
-//         ),
-//       );
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text('This item is already in the cart.'),
-//           duration: Duration(seconds: 2),
-//         ),
-//       );
-//     }
-//
-//     print("add ${newItem.title}");
-//     for (var item in cartlist.value) {
-//       print("old data : ${item.title}");
-//     }
-//   }
-//
-//
-//   removeFromCart(BuildContext context, String id, String name) {
-//     cartlist.value.removeWhere((item) => item.id == id);
-//
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       SnackBar(
-//         content: Text('$name telah dihapus dari keranjang.'),
-//         duration: Duration(seconds: 2),
-//       ),
-//     );
-//
-//     print("remove $name");
-//     for (var item in cartlist.value) {
-//       print("old data : ${item.title}");
-//     }
-//
-//     isload.value = !isload  .value;
-//   }
-//
-// }
-//
-//
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pas_kelas11/data,method,dll/EcommerceListModel.dart';
+
+class CartController extends GetxController{
+  RxList<Ecommercelistmodel> cartlist = <Ecommercelistmodel>[].obs;
+  RxBool isload = false.obs;
+  var isGopayChecked = false.obs;
+
+  void changeGopayValue(bool value) {
+    isGopayChecked.value = value;
+
+  }
+
+  // double updateTotalPrice(BuildContext context, double totalPrice){
+  //   for (var item in cartlist) {
+  //     totalPrice += (item.price ?? 0.0) * (item.quantity ?? 1); // Calculate total price based on item prices and quantities
+  //   }
+  //   return totalPrice;
+  // }
+
+  addToCart(BuildContext context, Ecommercelistmodel newItem) {
+    bool isExisting = false;
+
+    for (var item in cartlist.value) {
+      if (item.id == newItem.id) {
+        isExisting = true;
+        break;
+      }
+    }
+
+    if (!isExisting) {
+      cartlist.value.add(newItem);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Item added to cart.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('This item is already in the cart.'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+
+    print("add ${newItem.title}");
+    for (var item in cartlist.value) {
+      print("old data : ${item.title}");
+    }
+  }
+
+
+  removeFromCart(BuildContext context, String id, String name) {
+    cartlist.value.removeWhere((item) => item.id == id);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('$name telah dihapus dari keranjang.'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+
+    print("remove $name");
+    for (var item in cartlist.value) {
+      print("old data : ${item.title}");
+    }
+
+    isload.value = !isload  .value;
+  }
+
+}
+
+
--- lib/Cart/cart_view.dart
+++ lib/Cart/cart_view.dart
@@ -9,23 +9,110 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double totalPrice = 0.0;
+    return _CartContent();
+  }
+}
+
+class _CartContent extends StatefulWidget {
+  @override
+  _CartContentState createState() => _CartContentState();
+}
+
+class _CartContentState extends State<_CartContent> {
+  List<CartItem> cartItems = [
+    CartItem(name: 'Item 1', price: 100000, quantity: 2),
+    CartItem(name: 'Item 2', price: 50000, quantity: 1),
+
+  ];
+
+  TextEditingController promoCodeController = TextEditingController();
+  String appliedPromoCode = '';
+
+
+  void applyPromoCode() {
+    setState(() {
+      appliedPromoCode = promoCodeController.text;
+
+    });
+  }
+
+
+  void proceedToCheckout(BuildContext context) {
+
+  }
+
+  double get totalDiscount {
+
+    return 0.0;
+  }
+
+  double get totalPrice {
+    double total = cartItems.fold(0, (sum, item) => sum + (item.price * item.quantity));
+
+    return total - totalDiscount;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: textbuild(text: 'My Cart', color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold, height: 0),
+        title: textbuild(
+          text: 'My Cart',
+          color: Colors.black,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
+          height: 0,
+        ),
       ),
       drawer: NavDrawer(),
       body: ListView(
         children: [
+          // Display cart items
+          Column(
+            children: cartItems
+                .map(
+                  (item) => ListTile(
+                title: Text(item.name),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Rp. ${item.price * item.quantity}'),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              if (item.quantity > 1) {
+                                item.quantity--;
+                              }
+                            });
+                          },
+                          icon: Icon(Icons.remove),
+                        ),
+                        Text(item.quantity.toString()),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              item.quantity++;
+                            });
+                          },
+                          icon: Icon(Icons.add),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+                .toList(),
+          ),
           Padding(
             padding: EdgeInsets.only(top: 60.0),
             child: Column(
               children: [
-
                 SizedBox(height: 20.0),
                 Column(
                   children: [
-
                     Padding(
                       padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 10.0),
                       child: Container(
@@ -49,32 +136,45 @@ class Cart extends StatelessWidget {
                                 ),
                               ),
                             ),
-
                             Container(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 20.0),
-                                child: ElevatedButton(
-                                  onPressed: () {
-
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    primary: Colors.white,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
+                              width: 350.0, // Adjust the width as needed
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(right: 8.0),
+                                      child: TextFormField(
+                                        controller: promoCodeController,
+                                        decoration: InputDecoration(
+                                          hintText: '',
+                                          hintStyle: TextStyle(color: Colors.grey),
+                                          border: InputBorder.none,
+                                        ),
+                                        style: TextStyle(color: Colors.white),
+                                      ),
                                     ),
                                   ),
-                                  child: Text(
-                                    'Apply',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16.0,
+                                  ElevatedButton(
+                                    onPressed: () => applyPromoCode(),
+                                    style: ElevatedButton.styleFrom(
+                                      primary: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10.0),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      'Apply',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16.0,
+                                      ),
                                     ),
                                   ),
-                                ),
+                                  SizedBox(width: 10.0),
+                                ],
                               ),
-                            )
+                            ),
                           ],
                         ),
                       ),
@@ -86,34 +186,85 @@ class Cart extends StatelessWidget {
           ),
         ],
       ),
+
       bottomNavigationBar: BottomAppBar(
-        height: MediaQuery.of(context).size.height * 0.16,
-        child:  Column(
+        height: MediaQuery.of(context).size.height * 0.20,
+        child: Column(
           children: [
             Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Total All items:',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Total All items:',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
                   ),
-                  Text(
-                    'Rp.300.000.000', // Sesuaikan teks yang diinginkan
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0,
-                    ),
+                ),
+                Text(
+                  'Rp. ${totalPrice.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
                   ),
-                ],
+                ),
+              ],
+            ),
+            // Display applied promo code and total discount
+            if (appliedPromoCode.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Applied Promo Code:',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Text(
+                      '$appliedPromoCode',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-
+            if (totalDiscount > 0)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Total Discount:',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    Text(
+                      'Rp. ${totalDiscount.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             Padding(
-              padding: const EdgeInsets.only( top: 5.0),
+              padding: const EdgeInsets.only(top: 5.0),
               child: Container(
                 width: double.infinity,
                 height: 70.0,
@@ -135,15 +286,12 @@ class Cart extends StatelessWidget {
                         ),
                       ),
                     ),
-
                     Container(
                       alignment: Alignment.centerLeft,
                       child: Padding(
                         padding: const EdgeInsets.only(right: 20.0),
                         child: ElevatedButton(
-                          onPressed: () {
-                            // Tambahkan aksi yang diinginkan ketika tombol "Apply" ditekan
-                          },
+                          onPressed: () => proceedToCheckout(context),
                           style: ElevatedButton.styleFrom(
                             primary: Colors.white,
                             shape: RoundedRectangleBorder(
@@ -153,13 +301,11 @@ class Cart extends StatelessWidget {
                           child: Icon(
                             Icons.navigate_next_rounded,
                             size: 40,
-
                             color: Colors.black,
                           ),
                         ),
                       ),
                     ),
-
                   ],
                 ),
               ),
@@ -167,7 +313,14 @@ class Cart extends StatelessWidget {
           ],
         ),
       ),
-
     );
   }
+}
+
+class CartItem {
+  final String name;
+  final double price;
+  int quantity;
+
+  CartItem({required this.name, required this.price, required this.quantity});
 }
